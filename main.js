a0fa31e2a1c07edfc4a07d52804aecd1d2fde22e
Lukasz Derek <lukasz.derek@gmail.com>
2018-06-11 12:48:02 +0200

    FZ9SS-17-back-to-top made 1st version of working button

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* **********NAVIGATION********** */
+
 $(function(){
     if( $(window).width() <= 767 ) {
         $('#menu li a').click(function(e){
@@ -16,3 +18,20 @@ $(function(){
         });
     });
 }*/
+
+/* **********GO UP BUTTON********** */
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
