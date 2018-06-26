aa82d7c262d9363873bd7a6e738cb2b6ae6f79e1
Lukasz Derek <lukasz.derek@gmail.com>
2018-06-26 12:29:57 +0200

    feature/FZSS-17 changed the jquery link and added full file

--- main.js
+++ main.js
@@ -31,7 +31,6 @@ $(document).ready(function(){
         }
     });
 
-    //Click event to scroll to top
     $('.scrollToTop').click(function(){
         $('html, body').animate({scrollTop : 0},800);
         return false;
