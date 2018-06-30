d7200cde42f46941f94ed8d0de2cd13e042babe7
Daniel Luty <your_email@example.com>
2018-06-30 06:40:30 +0200

    FZ9SS-9-nav inks highlighted

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* ************* MOBILE - NAVIGATION ************* */
+/* ********************************* MOBILE - NAVIGATION *** */
 
 $('.nav-link').on('click', function () {
     $('.navbar-collapse').collapse('hide');
@@ -8,7 +8,7 @@ $('#topBtn').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
 
-/* *****************SHRINKING NAV ON SCROLL**** */
+/* ***************************** SHRINKING NAV ON SCROLL *** */
 
 $(document).ready(function () {
     $(window).scroll(function () {
@@ -43,9 +43,51 @@ $(document).ready(function () {
 });
 */
 
-/* **SMOOTH SCROLLING & NAV LINK HIGHLIGHTING** */
+/* ******************************* NAV LINK HIGHLIGHTING *** */
 
-/* *****************GO UP BUTTON*************** */
+// Cache selectors
+
+let lastId,
+    topMenu = $("#menu"),
+    topMenuHeight = topMenu.outerHeight() + 15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function () {
+        let item = $($(this).attr("href"));
+        if (item.length) {
+            return item;
+        }
+    });
+
+// Bind to scroll
+
+$(window).scroll(function () {
+    // Get container scroll position
+    let fromTop = $(this).scrollTop() + topMenuHeight;
+
+    // Get id of current scroll item
+    let cur = scrollItems.map(function () {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    let id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href='#" + id + "']").parent().addClass("active");
+    }
+});
+
+/* ************************************ SMOOTH SCROLLING *** */
+
+
+/* **************************************** GO UP BUTTON *** */
 
 $(document).ready(function () {
 
