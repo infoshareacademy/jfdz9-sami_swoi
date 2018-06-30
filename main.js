006e007687228ff3e1beb843a8cc8bf7fcf2c587
Daniel Luty <your_email@example.com>
2018-06-30 05:44:19 +0200

    FZ9SS-16-nav is shrinking & cleaned some code

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-/* *****************NAVIGATION***************** */
+/* ************* MOBILE - NAVIGATION ************* */
 
-$('.nav-link').on('click', function(){
+$('.nav-link').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
 
-$('#topBtn').on('click', function(){
+$('#topBtn').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
 
 /* *****************SHRINKING NAV ON SCROLL**** */
+
 $(document).ready(function () {
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 50) {
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 100) {
             $('.navbar').addClass('shrink');
         } else {
             $('.navbar').removeClass('shrink');
@@ -19,11 +20,36 @@ $(document).ready(function () {
     });
 });
 
+/*
+/!* SCROLLING BACK TO TOP ON RELOAD *!/
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
+*/
+/*
+/!* NAV NOT CHANGING ON RELOAD*!/
+$(window).scroll(ScrollChange);
+
+function ScrollChange() {
+    if ($(document).scrollTop() > 100) {
+        $('.navbar').addClass('shrink');
+    } else {
+        $('.navbar').removeClass('shrink');
+    }
+}
+
+$(document).ready(function () {
+    ScrollChange();
+});
+*/
+
+/* **SMOOTH SCROLLING & NAV LINK HIGHLIGHTING** */
+
 /* *****************GO UP BUTTON*************** */
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 10) {
             $('#topBtn').fadeIn('slow');
         } else {
@@ -31,15 +57,9 @@ $(document).ready(function(){
         }
     });
 
-    $('#topBtn').click(function(){
-        $('html, body').animate({scrollTop: 0},800);
+    $('#topBtn').click(function () {
+        $('html, body').animate({scrollTop: 0}, 800);
         return false;
     });
 
 });
-
-/* *****************SMOOTH SCROLLING*********** */
-
-var scroll = new SmoothScroll('a[href*="#"]');
-
-
