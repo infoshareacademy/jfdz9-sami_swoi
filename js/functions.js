960327a5d5d6cbf74e8d45e40a99f026622a7bc4
Lukasz Derek <lukasz.derek@gmail.com>
2018-07-26 13:38:51 +0200

    feature/FZ9SS-2 changed all imgs effects

--- js/functions.js
+++ js/functions.js
@@ -3,18 +3,71 @@
 $('.time-waste-img').mouseenter(function () {
     if ($(this).hasClass('1')) {
         $(this)
-            .fadeOut(400, function () {
-                $(this).attr('src', 'img/time-waste2.jpg');
+            .fadeOut(200, function () {
+                $(this).attr('src', 'img/time-waste.jpeg');
                 $(this).removeClass('1');
             })
-            .fadeIn(400);
+            .fadeIn(200);
     } else {
         $(this)
-            .fadeOut(400, function () {
-                $(this).attr('src', 'img/time-waste.jpeg');
+            .fadeOut(200, function () {
+                $(this).attr('src', 'img/time-waste2.jpg');
                 $(this).addClass('1');
             })
-            .fadeIn(400);
+            .fadeIn(200);
     }
 });
 
+$('.searching-img').mouseenter(function () {
+    if ($(this).hasClass('1')) {
+        $(this)
+            .fadeOut(200, function () {
+                $(this).attr('src', 'img/searching.jpeg');
+                $(this).removeClass('1');
+            })
+            .fadeIn(200);
+    } else {
+        $(this)
+            .fadeOut(400, function () {
+                $(this).attr('src', 'img/searching2.jpg');
+                $(this).addClass('1');
+            })
+            .fadeIn(200);
+    }
+});
+
+$('.clear-requirements-img').mouseenter(function () {
+    if ($(this).hasClass('1')) {
+        $(this)
+            .fadeOut(200, function () {
+                $(this).attr('src', 'img/clear-requirements.jpeg');
+                $(this).removeClass('1');
+            })
+            .fadeIn(200);
+    } else {
+        $(this)
+            .fadeOut(200, function () {
+                $(this).attr('src', 'img/clear-requrements2.jpg');
+                $(this).addClass('1');
+            })
+            .fadeIn(200);
+    }
+});
+
+$('.send-cv-img').mouseenter(function () {
+    if ($(this).hasClass('1')) {
+        $(this)
+            .fadeOut(200, function () {
+                $(this).attr('src', 'img/send-cv.jpeg');
+                $(this).removeClass('1');
+            })
+            .fadeIn(200);
+    } else {
+        $(this)
+            .fadeOut(400, function () {
+                $(this).attr('src', 'img/send-cv2.jpg');
+                $(this).addClass('1');
+            })
+            .fadeIn(200);
+    }
+});
