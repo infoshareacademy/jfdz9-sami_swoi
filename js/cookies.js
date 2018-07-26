f7ee6deb02aa0cf90ceb3abf9b4d38cfabd16ae3
Lukasz Derek <lukasz.derek@gmail.com>
2018-07-26 10:19:04 +0200

    feature/FZ9SS-11 made cookies logic

--- js/cookies.js
+++ js/cookies.js
@@ -1,5 +1,21 @@
 (function () {
+    var actualDate = (new Date().getTime()) / 1000;
+    var expTime = actualDate - localStorage.getItem('cookie date');
+
     $('.cookies-akcept').click(function() {
-        $('.cookies').fadeOut()
-    })
+        var cookieDate = (new Date().getTime()) / 1000;
+        $('.cookies').fadeOut();
+        localStorage.setItem('cookie', 'saved');
+        localStorage.setItem('cookie date', 'cookieDate');
+    });
+
+    if (expTime > 10000000000000000) {
+        $('.cookies').show();
+        localStorage.setItem('cookie', 'empty');
+    }
+
+    if (localStorage.getItem('cookie') === 'saved') {
+        $('.cookies').hide();
+    }
+
 })();
