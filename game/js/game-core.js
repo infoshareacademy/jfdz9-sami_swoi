8d53f62c076855aa7e69bddefe93a2e4ce343b06
zbooyx <zbooyx@gmail.com>
2018-08-26 03:19:29 +0200

    first obstacle approved

--- game/js/game-core.js
+++ game/js/game-core.js
@@ -10,7 +10,7 @@ function startGame() {
     gameArea.start();
     myGameCloudSmall = new component(100, 70, "img/cloud-1.png", 600, 50, 'img');
     myGameCloudBig = new component(100, 70, "img/cloud-2.png", 600, 30, 'img');
-    myGameObstacle = new component(10, 70, "red", 500, 303, 'obstacle');
+    myGameObstacle = new component(50, 70, "img/obstacle1.png", 500, 303, 'obstacle');
     myBackgroundBack = new component(600, 400, "img/country-back.png", 0, 0, 'background');
     myBackgroundForest = new component(600, 200, "img/country-forest.png", 0, 160, 'background');
     myBackgroundRoad = new component(600, 200, "img/country-road.png", 0, 200, 'background');
@@ -19,7 +19,7 @@ function startGame() {
 /** Funkcja która tworzy komponenty **/
 function component(width, height, color, x, y, type) {
     this.type = type;
-    if (type === 'img' || type === 'background') {
+    if (type === 'img' || type === 'background' || type === 'obstacle' ) {
         this.image = new Image();
         this.image.src = color;
     }
@@ -31,7 +31,7 @@ function component(width, height, color, x, y, type) {
     this.y = y;
     this.update = function () {
         ctx = gameArea.context;
-        if (type === "img" || type === "background") {
+        if (type === "img" || type === "background" || type === 'obstacle') {
             ctx.drawImage(this.image,
                 this.x, this.y, this.width, this.height);
             if (type === "background") {
@@ -54,6 +54,10 @@ function component(width, height, color, x, y, type) {
             if (this.x <= -(this.width)) {
                 this.x = 700;
             }
+        } else if (this.type === "obstacle") {
+            if (this.x <= -(this.width)) {
+                this.x = 700;
+            }
         }
     }
 
@@ -72,6 +76,9 @@ var gameArea = {
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
     }
 };
 
