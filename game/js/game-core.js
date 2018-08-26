10115f559898e5971b5954424cede36f7e353573
Łukasz Derek <lukasz.derek@gmail.com>
2018-08-26 17:26:38 +0200

    Poprawki na prezentacje

--- game/js/game-core.js
+++ game/js/game-core.js
@@ -1,6 +1,5 @@
 var myGameCloudSmall;
 var myGameCloudBig;
-var myGameObstacle;
 var myBackgroundBack;
 var myBackgroundForest;
 var myBackgroundRoad;
@@ -8,7 +7,7 @@ var gameSpeed = 10;
 
 setInterval(function () {
     gameSpeed *= 1.1;
-    console.log(gameSpeed);
+    // console.log(gameSpeed);
 }, 100);
 
 /** Startowanie gry i stworzenie jednego komponentu **/
@@ -16,7 +15,6 @@ function startGame() {
     gameArea.start();
     myGameCloudSmall = new component(100, 70, "img/cloud-1.png", 600, 50, 'img');
     myGameCloudBig = new component(100, 70, "img/cloud-2.png", 600, 30, 'img');
-    myObstacle = new component(50, 70, "img/obstacle1.png", 500, 303, 'obstacle');
     myBackgroundBack = new component(600, 400, "img/country-back.png", 0, 0, 'background');
     myBackgroundForest = new component(600, 200, "img/country-forest.png", 0, 160, 'background');
     myBackgroundRoad = new component(600, 200, "img/country-road.png", 0, 200, 'background');
@@ -25,7 +23,7 @@ function startGame() {
 /** Funkcja która tworzy komponenty **/
 function component(width, height, color, x, y, type) {
     this.type = type;
-    if (type === 'img' || type === 'background' || type === 'obstacle') {
+    if (type === 'img' || type === 'background') {
         this.image = new Image();
         this.image.src = color;
     }
@@ -37,7 +35,7 @@ function component(width, height, color, x, y, type) {
     this.y = y;
     this.update = function () {
         ctx = gameArea.context;
-        if (type === "img" || type === "background" || type === 'obstacle') {
+        if (type === "img" || type === "background") {
             ctx.drawImage(this.image,
                 this.x, this.y, this.width, this.height);
             if (type === "background") {
@@ -60,28 +58,8 @@ function component(width, height, color, x, y, type) {
             if (this.x <= -(this.width)) {
                 this.x = 700;
             }
-        } else if (this.type === "obstacle") {
-            if (this.x <= -(this.width)) {
-                this.x = 700;
-            }
         }
     }
-    this.crashWith = function(otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
-        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-            crash = false;
-        }
-        return crash;
-    }
-
 }
 
 
@@ -104,9 +82,6 @@ var gameArea = {
 
 /** Odświeżanie gry **/
 function updateGameArea() {
-    if (myGamePiece.crashWith(myObstacle)) {
-        myGameArea.stop();
-    } else {
         gameArea.clear();
         myBackgroundBack.speedX = -0.2;
         myBackgroundForest.speedX = -0.5;
@@ -119,12 +94,8 @@ function updateGameArea() {
         myBackgroundRoad.update();
         myGameCloudSmall.x -= 0.8; //to odpowiada za przesuwanie się chmurki (komponentu), próba
         myGameCloudBig.x -= 0.4; //to odpowiada za przesuwanie się chmurki (komponentu), próba
-        myGameObstacle.x -= 1; //to odpowiada za przesuwanie się chmurki (komponentu), próba
-        myGameObstacle.update();
         myGameCloudSmall.update();
         myGameCloudBig.update();
         myGameCloudBig.newPos();
         myGameCloudSmall.newPos();
-        myGameObstacle.newPos();
-    }
 }
